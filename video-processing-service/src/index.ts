b5a2927faa61b3afa5db711b5b63db40585fa689
Michael Wang <a0416517630@icloud.com>
2025-01-15 23:25:38 +1100

    initialised project

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,16 +1,13 @@
 import express from "express";
-import ffmpeg from "fluent-ffmpeg"; // this is just a Command Line Tool/Wrapper, the computer also needs to install ffmpeg
 
-// create an instance of express
 const app = express();
 const port = 3000;
 
-// specify a root path 
-app.get("/", (req, res) => { // every endpoint (url) has a request and response
-    res.send("Hello World!");
+// when the root url is accessed, a response string is sent back to the user
+app.get("/", (req, res) => {
+    res.send("Hello World");
 })
 
-// start server 
-app.listen(port, () => { // we are listening on this port and execute the following code 
-    console.log(`Video processing service listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
 })
